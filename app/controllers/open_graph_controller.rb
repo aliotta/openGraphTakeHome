96ca7558f1a949719472908938d65a6e2cc97f54
Alex <alexander.liotta@zumepizza.com>
2020-04-26 10:50:59 -0700

    use open-uri instead of net/http and parse html with Nokogiri

--- app/controllers/open_graph_controller.rb
+++ app/controllers/open_graph_controller.rb
@@ -3,32 +3,17 @@ class OpenGraphController < ApplicationController
         puts "Honey, I'm home!"
     end
     def submit_url
-        require 'net/http'
+        require 'open-uri'
         puts "URL Submitted"
         puts params
         Thread.new do
             Rails.application.executor.wrap do
-                dom = get_url(params[:url])
-                puts dom
+                #dom = get_url(params[:url])
+                #puts dom
+                page = Nokogiri::HTML(open(params[:url]))   
+                puts page
+                #extract_metadata(dom, "og:image")
             end
         end
     end
-
-    def get_url (uri_str, limit = 10) 
-        url = URI.parse(uri_str)
-        puts url
-
-        req = Net::HTTP::Get.new(url.to_s)
-        response = Net::HTTP.start(url.host, url.port) {|http|
-            http.request(req)
-        }
-        case response
-        when Net::HTTPSuccess     then response.body
-        when Net::HTTPRedirection then get_url(response['location'], limit - 1)
-        else
-            response.error!
-        end
-    end
-    
-
 end
