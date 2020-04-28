3062a89c992c4b6bd1ca982ba59824f93117747b
Alex <alexander.liotta@zumepizza.com>
2020-04-28 08:48:39 -0700

    validate url

--- app/controllers/open_graph_controller.rb
+++ app/controllers/open_graph_controller.rb
@@ -6,16 +6,19 @@ class OpenGraphController < ApplicationController
         Thread.new do
             Rails.application.executor.wrap do
                 page = Nokogiri::HTML(open(params[:url]))
-                puts page
                 metatag = page.at('meta[property="og:image"]')
                 if metatag and metatag['content']
+                    content = metatag['content']
                     begin
-                        ActiveRecord::Base.connection.execute("INSERT INTO open_graph_image (url) values ('#{ActiveRecord::Base.sanitize_sql(metatag['content'])}');")
+                        validateUrl = open(content).status
+                        ActiveRecord::Base.connection.execute("INSERT INTO open_graph_image (url) values ('#{ActiveRecord::Base.sanitize_sql(content)}');")
                         puts "Image processing in progress"
                         sleep 1;
                         #processing image
-                        ActiveRecord::Base.connection.execute("UPDATE open_graph_image SET status = 'processed' where url = '#{ActiveRecord::Base.sanitize_sql(metatag['content'])}';")
-                        ActionCable.server.broadcast "open_image_processing_channel", { status: "processed", url: metatag['content']}
+                        ActiveRecord::Base.connection.execute("UPDATE open_graph_image SET status = 'processed' where url = '#{ActiveRecord::Base.sanitize_sql(content)}';")
+                        ActionCable.server.broadcast "open_image_processing_channel", { status: "processed", url: content}
+                    rescue Errno::ENOENT => e
+                        ActionCable.server.broadcast "open_image_processing_channel", { status: "error", message: "The site has an og:image tag gthat is invalid #{content}"}
                     rescue StandardError => e
                         ActionCable.server.broadcast "open_image_processing_channel", { status: "error", message: "There was an error processing your image"}
                     end
